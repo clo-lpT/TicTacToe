3986c40fc1870780f4905d736b4f5b2328b140a3
haseunghee0207 <v32@vfunding.co.kr>
2022-11-10 14:31:24 +0900

    번갈아가면서 두기까지 성공

--- src/App.js
+++ src/App.js
@@ -11,59 +11,59 @@ function App() {
     for (let i = 0; i < 9; i++) {
       arr.push(null);
     }
+    // const arr = new Array(9).map((x) => null);
     setGameArray(arr);
-    // showArray();
+    console.log(gameArray);
   }, []);
-
-  useEffect(() => {
-    console.log('호출되냐')
-    gameArray.map((item, index) => {
-      console.log('hihi', index, item)
-      document.getElementById(1).innerHTML = item;
-    })
-    DrawGame();
-  }, [gameArray]);
 
   //칸을 눌렀을때 유저 표시가 나타남
   const drawUser = (position) => {
-    console.log('처음', gameArray)
-    console.log(gameArray[`${position}`]);
     if (gameArray[`${position}`] === null) {
-      console.log(2);
       setUser(!user);
-      console.log(3);
       user === true
         ? setGameArray(...gameArray, (gameArray[`${position}`] = "O"))
         : setGameArray(...gameArray, (gameArray[`${position}`] = "X"));
-      console.log(gameArray,'gameArray')
-    } else {
-      console.log(4);
-      console.log(gameArray[`${position}`]);
     }
     setGameArray(gameArray)
-    console.log(gameArray, "끝");
   };
 
   const DrawGame = () => {
-    console.log('Ddfdfjdf')
     return (
       <div>
         <table>
           <tbody>
             <tr>
-              <td id="1" onClick={() => drawUser(0)}></td>
-              <td id="2" onClick={() => drawUser(1)}></td>
-              <td id="3" onClick={() => drawUser(2)}></td>
+              <td id="1" onClick={() => drawUser(0)}>
+                {gameArray[0]}
+              </td>
+              <td id="2" onClick={() => drawUser(1)}>
+                {gameArray[1]}
+              </td>
+              <td id="3" onClick={() => drawUser(2)}>
+                {gameArray[2]}
+              </td>
             </tr>
             <tr>
-              <td id="4" onClick={() => drawUser(3)}></td>
-              <td id="5" onClick={() => drawUser(4)}></td>
-              <td id="6" onClick={() => drawUser(5)}></td>
+              <td id="4" onClick={() => drawUser(3)}>
+                {gameArray[3]}
+              </td>
+              <td id="5" onClick={() => drawUser(4)}>
+                {gameArray[4]}
+              </td>
+              <td id="6" onClick={() => drawUser(5)}>
+                {gameArray[5]}
+              </td>
             </tr>
             <tr>
-              <td id="7" onClick={() => drawUser(6)}></td>
-              <td id="8" onClick={() => drawUser(7)}></td>
-              <td id="9" onClick={() => drawUser(8)}></td>
+              <td id="7" onClick={() => drawUser(6)}>
+                {gameArray[6]}
+              </td>
+              <td id="8" onClick={() => drawUser(7)}>
+                {gameArray[7]}
+              </td>
+              <td id="9" onClick={() => drawUser(8)}>
+                {gameArray[8]}
+              </td>
             </tr>
           </tbody>
         </table>
